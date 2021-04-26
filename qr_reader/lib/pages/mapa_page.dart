6669df3570cb8b73364f348941791785c1ce3794
Gary Calle <Gary@DESKTOP-SH7FT1O>
2021-04-25 22:23:10 -0500

    agregado google maps

--- qr_reader/lib/pages/mapa_page.dart
+++ qr_reader/lib/pages/mapa_page.dart
@@ -1,19 +1,44 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'package:qr_reader/providers/db_provider.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
   const MapaPage({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  _MapaPageState createState() => _MapaPageState();
+}
 
+class _MapaPageState extends State<MapaPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
+
+    final CameraPosition puntoInicial = CameraPosition(
+      target: scan.getLatLng(),
+      zoom: 16,
+    );
+
+    Set<Marker> markers = new Set<Marker>();
+    markers.add(new Marker(markerId: MarkerId('geo-loc'), position: scan.getLatLng()));
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
       ),
-      body: Center(
-        child: Text(scan.valor),
+      body: GoogleMap(
+        markers: markers,
+        mapType: MapType.normal,
+        zoomControlsEnabled: false,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
